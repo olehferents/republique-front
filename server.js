ed429e3d030bb7b2a0190fda9c96da6ffd2791ce
Oleh Ferents <olegferenc1@gmail.com>
2020-12-08 09:37:14 +0200

    Change folder

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
